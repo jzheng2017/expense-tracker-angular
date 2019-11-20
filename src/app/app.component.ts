e39ef99aed7986bd9a8946ade0613bed322b07f6
Jiankai Zheng <jk.zheng@hotmail.com>
2019-11-20 22:30:10 +0100

    added login component with complete view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'expenses-tracker';
+  body;
+
+  constructor(private http: HttpClient) {
+    http.get('http://localhost:8080/users', {responseType: 'text'}).subscribe(body => this.body = body);
+  }
 }
